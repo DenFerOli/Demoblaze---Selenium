5401dfa859433604b31a2db3ec57fb369a3540dd
Denis Fernando <93955038+DenFerOli@users.noreply.github.com>
2026-04-28 22:45:25 -0300

    Create first file

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Demoblaze - Selenium
 Automation from site demoblaze with Selenium
+
+Python 3.14.4 ``` python --version ```
+pip 26.1 + ``` pip --version ```
---
+++ teste.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get('https://www.demoblaze.com/')
+
+time.sleep(10)
