7396936f4604cce0cc03f9c3ea4ab70a3c1bda55
Denis Fernando <93955038+DenFerOli@users.noreply.github.com>
2026-05-05 22:36:43 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -7,4 +7,19 @@ driver = webdriver.Chrome()
 driver.maximize_window()
 driver.get('https://www.demoblaze.com/')
 
-time.sleep(10)
+signin_button = driver.find_element(By.ID, 'signin2')
+sign_username = driver.find_element(By.ID, 'sign-username')
+sign_password = driver.find_element(By.ID, 'sign-password')
+signin_button.click()
+
+time.sleep(1)
+
+sign_username.send_keys('qa_tester')
+sign_username.send_keys(Keys.TAB)
+sign_password.send_keys('!@#teste')
+
+sign_up_button = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div[3]/button[2]')
+
+sign_up_button.click()
+
+time.sleep(5)
